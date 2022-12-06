59c83b67e2b939687867961e219a3d42043ceffd
Fran <fran@frannn.dev>
2022-12-07 01:35:58 +0800

    feat: setup prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: [""],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  root: true,
+    env: {
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    ignorePatterns: ['webpack.*.js', 'dist'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
 };
