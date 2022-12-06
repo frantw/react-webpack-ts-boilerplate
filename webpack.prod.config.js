840032cbcc99f3190aa02bb72c9d73df589ee92d
Fran <fran@frannn.dev>
2022-12-06 21:15:28 +0800

    feat: setup terser in webpack.prod.config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,15 @@
 const commonConfig = require("./webpack.config");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   ...commonConfig,
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
 };
