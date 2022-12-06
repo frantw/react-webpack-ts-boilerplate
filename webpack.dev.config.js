f0495b216d2fe0f423e16e9ea88395e7009e84f7
Fran <fran@frannn.dev>
2022-12-06 20:55:57 +0800

    feat: setup static option of devServer in webpack.dev.config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const commonConfig = require("./webpack.config");
 
 module.exports = {
   ...commonConfig,
   devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     hot: true,
   },
   mode: "development",
