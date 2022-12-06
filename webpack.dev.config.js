2d3dbfc212f0f800209b74db017ef0b71bc5e4db
Fran <fran@frannn.dev>
2022-12-06 21:43:47 +0800

    feat: setup bundle-analyzer in webpack.dev.config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
 const commonConfig = require("./webpack.config");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   ...commonConfig,
   devtool: "inline-source-map",
+  plugins: [
+    ...commonConfig.plugins,
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
