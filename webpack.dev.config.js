ea00ad6a579d14388008d06f50c808c57c11eb3a
Fran <fran@frannn.dev>
2022-12-06 20:57:48 +0800

    feat: setup inline-source-map devtool in webpack.dev.config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,6 +3,7 @@ const commonConfig = require("./webpack.config");
 
 module.exports = {
   ...commonConfig,
+  devtool: "inline-source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
